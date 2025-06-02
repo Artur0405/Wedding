3c22a5f9cfe172831aa7bbf7dcede1ea3d11accd
Arthur <artur.mkrtchyan0405@gmail.com>
2025-06-03 03:53:29 +0400

    font changed

--- HasmikScript.js
+++ HasmikScript.js
@@ -16,7 +16,7 @@ setFixedVh();
 setFixedVw();
 
 let screenHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--vh')) * 100;
-let virtualPageHeight = screenHeight * 5.7; // или 4, 5 — сколько "прокрутки" нужно
+let virtualPageHeight = screenHeight * 3.7; // или 4, 5 — сколько "прокрутки" нужно
 
 if (window.innerWidth <= 768) {
   document.getElementById("scroll-wrapper").style.height = virtualPageHeight * 1.15 + "px";
